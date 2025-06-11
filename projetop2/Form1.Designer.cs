bb44a45efdf26edd0241230bbe98a8b47fc7bffe
Joao Vitor <joaovitor.oliveira@sou.unifeb.edu.br>
2025-06-11 08:26:01 -0300

    ADICIONADO 2 TEXTBOX + UM BUTTON E 2 LABEL + ARQUIVO CSV USER E SENHA

--- projetop2/Form1.Designer.cs
+++ projetop2/Form1.Designer.cs
@@ -28,18 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtUser = new TextBox();
+            txtSenha = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnValidar = new Button();
             SuspendLayout();
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(237, 77);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(242, 23);
+            txtUser.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(237, 141);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(242, 23);
+            txtSenha.TabIndex = 1;
+            txtSenha.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(237, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(237, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Senha";
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(309, 201);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(75, 41);
+            btnValidar.TabIndex = 4;
+            btnValidar.Text = "Entrar";
+            btnValidar.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnValidar);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUser);
             Name = "LoginForm";
             Text = "Login";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUser;
+        private TextBox txtSenha;
+        private Label label1;
+        private Label label2;
+        private Button btnValidar;
     }
 }
